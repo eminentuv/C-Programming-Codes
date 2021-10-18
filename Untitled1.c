1724c54f565a7715a61205bc6b69e67258fa4d2a
eminentuv <92053843+eminentuv@users.noreply.github.com>
2021-10-18 21:15:23 +0530

    First C programme

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	int b=2;
+	int c;
+	printf("Please Enter Value to be multiplied\n");
+	scanf("%d", &a);
+	printf("\n%d",c);
+	printf("\nHello World");
+	printf("\n%d + %d = %d",a,b,a+b);
+	printf("\n%d - %d = %d",a,b,a-b);
+	printf("\n%d * %d = %d",a,b,a*b);
+	printf("\n%d / %d = %d",a,b,a/b);
+	printf("\n%d / %d = %f",a,b,(float)a/b);
+}
